6a691b85fa75acef13a49cf1387158825bbe10fb
tuan30102000 <vandathd49@gmail.com>
2021-07-22 10:26:04 +0700

    sadá

--- src/features/form/component/FormCp/index.jsx
+++ src/features/form/component/FormCp/index.jsx
@@ -2,24 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InputField from '../../../../Component/inputField';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+const schema = yup.object().shape({
+    content: yup.string().required('phai nhap noi dung'),
+});
 
 FormCp.propTypes = {
 
 };
 
 function FormCp(props) {
-    const form =useForm({
-        defaultValues:{
-            content:''
-        }
+    const form = useForm({
+        defaultValues: {
+            content: ''
+        },
+        resolver: yupResolver(schema)
     })
-    const handlesubmit1=function(value){
-     console.log('data is',value)       
+    const handlesubmit1 = function (value) {
+
+        console.log('data is', value)
     }
     return (
-       
-        <form  onSubmit={form.handleSubmit(handlesubmit1)}>
-            <InputField form={form} name='content' label='todo' form={form}/>
+
+        <form onSubmit={
+            form.handleSubmit(handlesubmit1)
+
+
+        }>
+            <InputField form={form} name='content' label='todo' form={form} />
         </form>
     );
 }
